7f4ab7b504b1c863499956e190c497235b7cde7b
example <mdizman124@gmail.com>
2024-02-20 18:20:34 +0300

    last added

--- app/components/home/Banner.tsx
+++ app/components/home/Banner.tsx
@@ -3,7 +3,7 @@ const Banner = () => {
   return (
     <div className="h-[237pxs] bg-black flex items-center justify-center">
       <div className="h-[137px] relative w-full">
-            <Image fill src={"/d.webp"} alt=""/>
+            <Image fill src={"/d.webp"} alt="" className="object-cover"/>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Banner from "./components/home/Banner";
 import Category from "./components/home/Category";
+import Products from "./components/home/Products";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
    <div>
     <Category/>
     <Banner/>
+    <Products/>
    </div>
   );
 }
